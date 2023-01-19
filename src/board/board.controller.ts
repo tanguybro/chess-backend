59000027ac6e44e39ceb6a6b2a4997eb14561315
tanguybro <tanguy.brouassin@gmail.com>
2023-01-19 11:15:24 +0100

    add random move if no best move

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios/dist';
-import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { Chess, Square } from '../../node_modules/chess.js/dist/chess';
 import { MoveDto } from './move.dto';
@@ -37,15 +37,19 @@ export class BoardController {
     @Post('/move')
     async move(@Body() moveDto: MoveDto) {
         console.log(moveDto);
-        try {
-            this.chess.move(moveDto.move);
-        } catch (error) {
-            console.log(error);
-            throw new HttpException('Invalid move', HttpStatus.INTERNAL_SERVER_ERROR);
-        }
 
-        console.log(await this.getIaMove(this.chess.fen()));
-        this.chess.move(await this.getIaMove(this.chess.fen()));
+        this.chess.move(moveDto.move);
+
+        const iaMove = await this.getIaMove(this.chess.fen());
+        console.log('IA move : ', iaMove);
+
+        // Random move if no best move
+        if (iaMove) {
+            this.chess.move(iaMove);
+        } else {
+            const moves = this.chess.moves();
+            this.chess.move(moves[Math.floor(Math.random() * moves.length)]);
+        }
 
         console.log(this.chess.ascii());
     }
@@ -54,6 +58,8 @@ export class BoardController {
         const url = 'https://www.chessdb.cn/cdb.php?action=querybest&board=' + fen + '&json=1';
 
         const { data } = await firstValueFrom(this.httpService.get(url));
+        console.log(data);
+
         return data.move;
     }
 }
