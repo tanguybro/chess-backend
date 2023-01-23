5af5d27f765261a5f97949d9f58dfb335f5dcc26
Tanguy Brouassin <tanguy.brouassin@atlasmusic.fr>
2023-01-23 11:23:35 +0100

    optimize best move

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios/dist';
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { Chess, Square } from '../../node_modules/chess.js/dist/chess';
 import { MoveDto } from './move.dto';
@@ -41,8 +41,6 @@ export class BoardController {
         this.chess.move(moveDto.move);
 
         const iaMove = await this.getIaMove(this.chess.fen());
-        console.log('IA move : ', iaMove);
-
         // Random move if no best move
         if (iaMove) {
             this.chess.move(iaMove);
@@ -50,17 +48,10 @@ export class BoardController {
             const moves = this.chess.moves();
             this.chess.move(moves[Math.floor(Math.random() * moves.length)]);
         }
-
-        console.log(this.chess.ascii());
-    }
-
-    @Get('/can-move')
-    allowedMoves(@Query() query): any[] {
-        console.log(query.position);
-        return this.chess.moves({ square: query.position as Square });
     }
 
     async getIaMove(fen: string): Promise<string> {
+        console.log(fen);
         const url = 'https://www.chessdb.cn/cdb.php?action=querybest&board=' + fen + '&json=1';
 
         const { data } = await firstValueFrom(this.httpService.get(url));
